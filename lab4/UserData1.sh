0e13319bc6407d849efed10b34689f5728a83a9f
igork0 <52069633+igork0@users.noreply.github.com>
2020-12-12 11:56:14 +0200

    Add files via upload

---
+++ lab4/UserData1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+sudo yum update -y
+sudo yum -y install httpd
+sudo service httpd start  
+sudo chkconfig httpd on
+sudo groupadd apache
+sudo usermod -a -G www ec2-user
+groups
+sudo chgrp -R apache /var/www
+sudo chmod 2775 /var/www
+find /var/www -type d -exec sudo chmod 2775 {} +
+find /var/www -type f -exec sudo chmod 0664 {} +                
+cd /var/www/html
+echo "<h1>Ihor Rygal Lab 4 instance 1</h1>" > index.html
